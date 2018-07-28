1cc68a4681d2eabc754404b03ce5568521016c09
unknown <eberleo@WKS0000620211.eu.boehringer.com>
2018-07-28 11:56:28 -0300

    constante Entregada nueva

--- src/Helper/Constant.php
+++ src/Helper/Constant.php
@@ -6,6 +6,7 @@ class Constant {
     const TO_BE_PROCESSED = "To be processed";
     const IN_PROCESS = "In process";
     const FINISHED = "Finished";
-
+    const DELIVERED = "Entregada";
+    
     const PENDING_FOR_APPROVAL = "Pending for approval";
 }

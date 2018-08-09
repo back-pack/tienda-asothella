af1ef0470951096427d4c5c56975a321665b10d0
Oscar Eberle <oscar.eberle.ext@boehringer-ingelheim.com>
2018-08-09 14:38:58 -0300

    creacion de migracion de esquema

---
+++ src/Migrations/Version20180809172842.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180809172842 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE product_request ADD CONSTRAINT FK_EA4E942F4584665A FOREIGN KEY (product_id) REFERENCES product (id)');
+        $this->addSql('CREATE INDEX IDX_EA4E942F4584665A ON product_request (product_id)');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE product_request DROP FOREIGN KEY FK_EA4E942F4584665A');
+        $this->addSql('DROP INDEX IDX_EA4E942F4584665A ON product_request');
+    }
+}

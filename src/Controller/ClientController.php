f8068ea4bc8e78bbb401ce2b6460d3eb6d2a06e7
unknown <eberleo@WKS0000620211.eu.boehringer.com>
2018-08-18 21:32:25 -0300

    solicitud lado cliente

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -7,12 +7,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+use App\Repository\ProductRepository;
 use App\Form\RequirementType;
+use App\Form\ProductRequestType;
 use App\Form\CompanyType;
 use App\Entity\Requirement;
 use App\Entity\ProductRequest;
 use App\Entity\Company;
-use App\Entity\RoofTile;
+use App\Entity\Product;
 use App\Helper\Constant;
 
 class ClientController extends Controller
@@ -88,72 +91,167 @@ class ClientController extends Controller
     }
 
     /**
-     * @Route("/client/requirement", name="requirement")
+     * @Route("/client/shopping", name="client_shopping")
      */
-    public function newRequirement(Request $request)
+    public function shopping(ProductRepository $productRepository, Session $cart)
     {
-        $productRequest = new ProductRequest();
-        $requirement = new Requirement();
-        $requirement->addProductRequest($productRequest);
-        $form = $this->createForm(RequirementType::class, $requirement);
-        $form->handleRequest($request);
+        if(null === $cart->getId()) {
+            $cart = new Session();
+            $cart->start();
+            $cart->setId(uniqid());
+        }
 
-        if($form->isSubmitted() && $form->isValid())
-        {
-            $em = $this->getDoctrine()->getManager();
-            $finalCost = null;
-            foreach($requirement->getProductRequests() as $product)
-            {
-                $tile = $em->getRepository(RoofTile::class)->findOneBy(['type' => $product->getType()]);
-                if(!$tile)
-                {
-                    $this->addFlash('danger', 'Un error ocurrió con el envío del formulario.');
-                    return $this->redirectToRoute('request');
+        $items = $cart->get('items');
+
+        return $this->render('shopping/index.html.twig', [
+            'products' => $productRepository->findAll(), 
+            'items' => $items
+            ]);
+    }
+
+    /**
+     * @Route("/client/shopping/additem/{uid}", name="client_shopping_additem")
+     */
+    public function addItem(Request $request, $uid, Session $cart)
+    {
+        if(null === ($cart->getId())) {
+            return $this->redirectToRoute('client_shopping');
+        }
+        $product = $this->getDoctrine()->getRepository(Product::class)->findOneBy(['uid' => $uid]);
+        if(!$product) {
+            $this->addFlash('danger', 'El producto no existe en la tienda.');
+            return $this->redirectToRoute('client_shopping');
+        }
+        $productRequest = new ProductRequest();
+        $form = $this->createForm(ProductRequestType::class, $productRequest);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $productRequest->setProduct($product);
+            
+            if(null !== $cart->get('items')) {
+                foreach($cart->get('items') as $item) {
+                    $cartProducts[md5(uniqid())] = $item;
                 }
-                $cost = $tile->getCost() * $product->getQuantity();
-                $product
-                    ->setCost($cost);
-                if(is_null($product->getRequirement()))
-                {
-                    $product->setRequirement($requirement);
-                }
-                $finalCost += $cost;
             }
             
-            $requirement
-                ->setFinalCost($finalCost)
-                ->setCreationDate(new \DateTime('today'))
-                ->setCompany($this->getUser())
-                ->setRequirementNumber(uniqid())
-                ->setStatus(Constant::TO_BE_APPROVED)
-                ;
+            $cartProducts[md5(uniqid())] = $productRequest;
+            $cart->set('items', $cartProducts);
             
-            $em->persist($requirement);
-            $em->flush();
-
-            $this->addFlash('success', 'Su solicitud es la numero: RQ'.$requirement->getId().'. En breve procesaremos su solicitud!');
-
-            return $this->redirectToRoute('client');
+            $this->addFlash('success', 'El producto fue agregado al carrito.');
+            return $this->redirectToRoute('client_shopping');
         }
-        return $this->render('client/newRequirement.html.twig', [
+        return $this->render('shopping/addItem.html.twig', [
             'form' => $form->createView(),
-            // 'prices' => $prices
+            'product' => $product,
         ]);
     }
 
     /**
-     * @Route("/client/shopping/viewcart", name="client_shopping_viewcart")
+     * @Route("/client/shopping/edititem/{itemId}", name="client_shopping_edititem")
      */
-    public function viewCart()
+    public function edititem(Session $cart, Request $request, $itemId)
     {
-        //TODO
-        $user = $this->getUser();
-        $requirements = $this->getDoctrine()->getRepository(Requirement::class)->findBy(['company' => $this->getUser()->getId()]);
+        if(null === ($cart->getId())) {
+            return $this->redirectToRoute('client_shopping');
+        }
+        $cartProducts = $cart->get('items');
+        $productReq = $cartProducts[$itemId];
 
-        return $this->render('client/index.html.twig', [
-            'user' => $user->getContactName(),
-            'requirements' => $requirements
+        $form = $this->createForm(ProductRequestType::class, $productReq);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $cartProducts[$itemId] = $productReq;
+            $cart->set('items', $cartProducts);
+
+            $this->addFlash('success', 'El producto fue modificado.');
+            
+            return $this->redirectToRoute('client_shopping');
+        }
+        return $this->render('shopping/addItem.html.twig', [
+            'form' => $form->createView(),
+            'edit' => true,
+            'product' => $productReq->getProduct(),
         ]);
+    }
+
+    /**
+     * @Route("/client/shopping/removeitem/{itemId}", name="client_shopping_removeitem")
+     */
+    public function removeitem(Session $cart, $itemId)
+    {
+        if(null === ($cart->getId())) {
+            return $this->redirectToRoute('client_shopping');
+        }
+        $cartProducts = $cart->get('items');
+        unset($cartProducts[$itemId]);
+
+        $cart->set('items', $cartProducts);
+
+        if(empty($cartProducts)) {
+            $cart->invalidate();
+            $this->addFlash('success', 'El carrito fue vaciado');
+        } else {
+            $this->addFlash('success', 'El producto fue removido.');
+        }
+        return $this->redirectToRoute('client_shopping');
+    }
+
+    /**
+     * @Route("/client/shopping/dropcart", name="client_shopping_dropcart")
+     */
+    public function dropCart(Session $cart)
+    {
+        if(null === ($cart->getId())) {
+            return $this->redirectToRoute('client_shopping');
+        }
+        $cart->invalidate();
+
+        $this->addFlash('success', 'El carrito fue vaciado.');
+
+        return $this->redirectToRoute('client_shopping');
+        
+    }
+
+    /**
+     * @Route("/client/requirement/new", name="client_requirement_new")
+     */
+    public function newRequirement(Session $cart)
+    {
+        if(null === ($cart->getId())) {
+            return $this->redirectToRoute('client_shopping');
+        }
+        
+        $cartItems = $cart->get('items');
+        $em = $this->getDoctrine()->getManager();
+        $products = $em->getRepository(Product::class)->findAll();
+        
+        $requirement = new Requirement();
+        $finalCost = null;
+        foreach($cartItems as $item) {
+            $index = array_search($item->getProduct(), $products);
+            $item->setProduct($products[$index]);
+            
+            $finalCost += $item->getProduct()->getPrice() * $item->getQuantity();
+            $requirement->addProductRequest($item);
+            $em->persist($item);
+        }
+
+        $requirement
+            ->setFinalCost($finalCost)
+            ->setCreationDate(new \DateTime('today'))
+            ->setRequirementNumber(md5(uniqid()))
+            ->setStatus(Constant::TO_BE_APPROVED)
+            ->setCompany($this->getUser())
+            ;
+        
+        $em->persist($requirement);
+        $em->flush();
+
+        $cart->invalidate();
+
+        $this->addFlash('success', 'Su solicitud es la numero: RQ'.$requirement->getId().'. En breve procesaremos su solicitud!');
+
+        return $this->redirectToRoute('client_index');
     }
 
     /**
